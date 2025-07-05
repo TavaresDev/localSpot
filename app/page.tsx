bc864b60a9c69da8d8a3f350a9d924213c53e1a2
AndreTavares <Vtavares.andre@gmail.com>
2025-07-04 22:22:34 -0400

    [01] page fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import FooterSection from "@/components/homepage/footer";
 import HeroSection from "@/components/homepage/hero-section";
 import Benefits from "@/components/homepage/benefits";
-import Integrations from "@/components/homepage/integrations";
+import Features from "@/components/homepage/integrations";
 import { getSubscriptionDetails } from "@/lib/subscription";
 import PricingTable from "./pricing/_component/pricing-table";
 
@@ -12,7 +12,7 @@ export default async function Home() {
     <>
       <HeroSection />
       <Benefits />
-      <Integrations />
+      <Features />
       <PricingTable subscriptionDetails={subscriptionDetails} />
       <FooterSection />
     </>
